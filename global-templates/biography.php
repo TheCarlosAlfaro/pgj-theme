69f970f6514748e5db896e0642e0e74599bdc226
devbysalas <rgkalin@gmail.com>
2019-03-13 11:59:02 -0700

    Adds ACF to template for gallery section

--- global-templates/biography.php
+++ global-templates/biography.php
@@ -7,36 +7,35 @@
  */
 
 if (!defined('ABSPATH')) {
-  exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 ?>
 
 <div id="home-biography">
-    <div class="hidden-sx section py-5">
-        <div class="text-center profile-pic">
-            <img src="http://staging.guillermojimenez.online/wp-content/uploads/2019/02/dad-ig.jpg" class="rounded-circle" alt="...">
-        </div>
-        <h2 class="text-center display-4 font-weight-bold">
-            <?php echo get_theme_mod('home_podcast_heading', 'Guillermo Jimenez'); ?>
-        </h2>
+  <div class="hidden-sx section py-5">
+    <div class="text-center profile-pic">
+      <img src="<?php the_field("profile_picture") ?>" class="rounded-circle" alt="...">
+    </div>
+    <h2 class="text-center display-4 font-weight-bold">
+      <?php the_field("profile_name") ?>
+    </h2>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md">
-                    <div class="one-col-text">
-                        <p>When Guillermo Jimenez – Global Founder and Senior Pastor of ACFC Church – set out to pioneer with 40 people and a school hall, he was unsure of the highs and lows, sacrifices and disappointments that would mark the 32-year journey of miraculous opportunities and dreams come true.</p>
-                        <p>Secretos de un lider extraordinario is his story, woven in to the greatest story ever told, with room for chapters all your own to be written and re-written. This book is a declaration to your future, a reminder that belief, passion and calling can give way to a full and abundant life, despite any mysteries, setbacks or mountains that get in the way.</p>
-                        <div class="text-center">
-                            <div>
-                                <p class="font-weight-bold">When it comes to life with Jesus, there is no doubt – your best is yet to come!</p>
-                            </div>
-                            <!-- <a href="<?php echo get_theme_mod('home_podcast_button_url', '#'); ?>">
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+          <div class="one-col-text">
+            <p><?php the_field("biography") ?></p>
+            <div class="text-center">
+              <div>
+                <p class="font-weight-bold"><?php the_field("the_vision") ?></p>
+              </div>
+              <!-- <a href="<?php echo get_theme_mod('home_podcast_button_url', '#'); ?>">
                           <button type="button" class="btn btn-outline-primary"><?php echo get_theme_mod('home_podcast_button_text', 'GET IT NOW'); ?></button>
                         </a> -->
-                        </div>
-                    </div>
-                </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div> 
+  </div>
+</div>
--- global-templates/gallery.php
+++ global-templates/gallery.php
@@ -7,31 +7,31 @@
  */
 
 if (!defined('ABSPATH')) {
-  exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 ?>
 
 <div id="home-gallery">
-    <div class="gallery">
-        <div class="grid">
-            <div class="cell">
-                <img src="http://guillermo-jimenez.local/wp-content/uploads/2019/02/IMG_9458.jpg" class="responsive-image">
-            </div>
-            <div class="cell">
-                <img src="http://guillermo-jimenez.local/wp-content/uploads/2019/02/IMG_0415.jpg" class="responsive-image">
-            </div>
-            <div class="cell">
-                <img src="http://guillermo-jimenez.local/wp-content/uploads/2019/02/IMG_6102.jpg" class="responsive-image">
-            </div>
-            <div class="cell">
-                <img src="http://guillermo-jimenez.local/wp-content/uploads/2019/02/IMG_0425.jpg" class="responsive-image">
-            </div>
-            <div class="cell">
-                <img src="http://guillermo-jimenez.local/wp-content/uploads/2019/02/IMG_9450.jpg" class="responsive-image">
-            </div>
-            <div class="cell">
-                <img src="http://guillermo-jimenez.local/wp-content/uploads/2019/02/IMG_0413.jpg" class="responsive-image">
-            </div>
-        </div>
+  <div class="gallery">
+    <div class="grid">
+      <div class="cell">
+        <img src="<?php the_field("image_1") ?>" class="responsive-image">
+      </div>
+      <div class="cell">
+        <img src="<?php the_field("image_2") ?>" class="responsive-image">
+      </div>
+      <div class="cell">
+        <img src="<?php the_field("image_3") ?>" class="responsive-image">
+      </div>
+      <div class="cell">
+        <img src="<?php the_field("image_4") ?>" class="responsive-image">
+      </div>
+      <div class="cell">
+        <img src="<?php the_field("image_5") ?>" class="responsive-image">
+      </div>
+      <div class="cell">
+        <img src="<?php the_field("image_6") ?>" class="responsive-image">
+      </div>
     </div>
-</div> 
+  </div>
+</div>
